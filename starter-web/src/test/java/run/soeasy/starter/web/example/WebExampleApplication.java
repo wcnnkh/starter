34f5c544548e096b516970d92cd54d258f7e3a07
wcnnkh <416078075@qq.com>
2025-06-08 15:59:53 +0800

    init

---
+++ starter-web/src/test/java/run/soeasy/starter/web/example/WebExampleApplication.java
@@ -0,0 +1,11 @@
+package run.soeasy.starter.web.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebExampleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WebExampleApplication.class, args);
+	}
+}
