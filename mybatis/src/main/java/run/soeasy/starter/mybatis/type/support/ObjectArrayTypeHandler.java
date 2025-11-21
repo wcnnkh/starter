088824c3b80e52bd6cd1197930bd40de4f3b177b
wcnnkh <416078075@qq.com>
2025-11-21 10:19:46 +0800

    cleanup

---
+++ mybatis/src/main/java/run/soeasy/starter/mybatis/type/support/ObjectArrayTypeHandler.java
@@ -0,0 +1,12 @@
+package run.soeasy.starter.mybatis.type.support;
+
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+
+import run.soeasy.starter.mybatis.type.JsonTypeHandler;
+
+@MappedTypes(Object[].class)
+@MappedJdbcTypes({ JdbcType.VARCHAR, JdbcType.LONGVARCHAR, JdbcType.NVARCHAR, JdbcType.LONGNVARCHAR })
+public class ObjectArrayTypeHandler extends JsonTypeHandler<Object[]> {
+}
